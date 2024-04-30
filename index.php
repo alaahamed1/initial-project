33c4a3ce32df02aa8c350d52b2452a11bd36c383
alaahamed1 <alaa.elzeftawy@gmail.com>
2024-04-30 21:41:20 +0300

    first commit

--- index.php
+++ index.php
@@ -1,5 +1,15 @@
 <?php
+if (isset($_POST['name'])) {
+    $name = $_POST['user_name'];
+    $email = $_POST['user_email'];
+    $phone = $_POST['user_pass'];
 
+
+    // data dase (my_project) doesn't exist yet
+    $connection = mysqli_connect('localhost','root','','my_project');
+    $sql = "INSERT INTO `contact` (`name`, `email`, `phone`) VALUES ('$name','$email','$phone')";
+    mysqli_query($connection,$sql);
+}
 
 ?>
 
@@ -8,14 +18,14 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>simple form</title>
+    <title>simple form app.</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/water.css@2/out/water.css">
 </head>
 <body>
     <form method="post" enctype="multipart/form-data">
         <input type="text" id="name" name="user_name" value=""/>
-        <input type="email" id="email" name="user_name" value=""/>
-        <input type="password" id="pass" name="user.pass" value=""/>
+        <input type="email" id="email" name="user_email" value=""/>
+        <input type="password" id="pass" name="user_pass" value=""/>
         <button type="submit">submit</button>
     </form>
 </body>
