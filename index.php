1396579da712f74bf44a93ccbffdded623ad202f
Eslamhatab <mr.hatab055@gmail.com>
2024-06-30 18:55:50 +0300

    Connection File

--- index.php
+++ index.php
@@ -1,18 +1,4 @@
-<?php
-if (isset($_POST['name'])) {
-    $name = $_POST['user_name'];
-    $email = $_POST['user_email'];
-    $phone = $_POST['user_pass'];
-
-
-    // database (my_project) doesn't exist yet
-    $connection = mysqli_connect('localhost','root','','my_project');
-    $sql = "INSERT INTO `contact` (`name`, `email`, `phone`) VALUES ('$name','$email','$phone')";
-    mysqli_query($connection,$sql);
-}
-
-?>
-
+@include('connection.php');
 <!DOCTYPE html>
 <html lang="en">
 <head>
