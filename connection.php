f28bf8dbc612453af3a0d634b5ec9ea9a6bcff24
alaahamed1 <alaa.elzeftawy@gmail.com>
2024-07-02 09:02:12 +0300

    Task - 1st session

--- connection.php
+++ connection.php
@@ -4,7 +4,6 @@ if (isset($_POST['name'])) {
     $email = $_POST['user_email'];
     $phone = $_POST['user_pass'];
 
-
     // database (my_project) doesn't exist yet
     $connection = mysqli_connect('localhost','root','','my_project');
     $sql = "INSERT INTO `contact` (`name`, `email`, `phone`) VALUES ('$name','$email','$phone')";
